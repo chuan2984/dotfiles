5929a5f4c7830ed8440095ba16143326adcd02a7
chuan2984 <chuanscareer@gmail.com>
2024-03-09 22:29:11 -0800

    fix: vim navigator mixed up mapping

--- .config/nvim/lua/plugins/navigator.lua
+++ .config/nvim/lua/plugins/navigator.lua
@@ -8,9 +8,9 @@ return {
     vim.keymap.set({ 'n', 't' }, '<C-j>', '<CMD>NavigatorDown<CR>')
     vim.keymap.set({ 'n', 't' }, '<C-;>', '<CMD>NavigatorPrevious<CR>')
 
-    vim.keymap.set({ 'n', 't' }, '<A-a>', '<c-w>+')
-    vim.keymap.set({ 'n', 't' }, '<A-d>', '<c-w>-')
-    vim.keymap.set({ 'n', 't' }, '<A-w>', '<c-w>>')
-    vim.keymap.set({ 'n', 't' }, '<A-s>', '<c-w><')
+    vim.keymap.set({ 'n', 't' }, '<A-w>', '<c-w>+')
+    vim.keymap.set({ 'n', 't' }, '<A-s>', '<c-w>-')
+    vim.keymap.set({ 'n', 't' }, '<A-a>', '<c-w>>')
+    vim.keymap.set({ 'n', 't' }, '<A-d>', '<c-w><')
   end,
 }
