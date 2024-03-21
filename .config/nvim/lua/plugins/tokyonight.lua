4bfbc073bedd165ffbe371f3a6a4df6b26a68a0c
chuan2984 <chuanscareer@gmail.com>
2024-03-20 17:00:42 -0700

    fix: so that it would only change theme if its in the obsidian folder

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -8,11 +8,18 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      -- Used to set filetype for untraditonally named files
+      vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+        group = vim.api.nvim_create_augroup('autoset theme tokyo', { clear = true }),
+        pattern = '*', -- Matches all files
+        callback = function()
+          local theme_name = 'tokyonight'
+          local is_not_obsidian_file = not vim.fn.expand('%:p'):lower():match 'obsidian'
+          if vim.g.colors_name ~= theme_name and is_not_obsidian_file then
+            vim.cmd.colorscheme 'tokyonight-night'
+          end
+        end,
+      })
     end,
   },
 }
