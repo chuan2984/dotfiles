d512a2ae102db7f2d92b614914152c310f820075
chuan2984 <chuanscareer@gmail.com>
2024-03-22 10:31:22 -0700

    feat: add chatgpt to nvim

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -28,6 +28,7 @@ return {
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = '[H]unk', _ = 'which_key_ignore' },
+        ['<C-g>'] = { name = '[G]pt', _ = 'which_key_ignore' },
       }
     end,
   },
