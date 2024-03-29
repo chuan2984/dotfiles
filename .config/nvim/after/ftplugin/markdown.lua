447b12567a8174f3adf503bb92361ef1a1d8bb1e
chuan2984 <chuanscareer@gmail.com>
2024-03-29 09:43:58 -0700

    fix(nvim): fix markdown theme to use transparent bg

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,9 @@
 -- options are dragon, wave, lotus
-require('kanagawa').load 'wave'
+require('kanagawa').setup {
+  theme = 'wave',
+  transparent = true,
+}
+
 vim.opt.wrap = true
 vim.api.nvim_win_set_option(0, 'colorcolumn', '120')
+vim.cmd 'colorscheme kanagawa'
