6e16238137b88454046ce4a32c0e0c09c2d8c512
chuan2984 <chuanscareer@gmail.com>
2024-05-03 11:53:01 -0700

    chore(ts): textobject plugins cleanup

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,6 @@
 return {
   'echasnovski/mini.nvim',
   dependencies = {
-    { 'nvim-treesitter/nvim-treesitter-textobjects', lazy = true },
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
@@ -45,22 +44,6 @@ return {
 
       -- Which character to use for drawing scope indicator
       symbol = '\u{e621}',
-    }
-
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [']quote
-    --  - ci'  - [C]hange [I]nside [']quote
-    local spec_treesitter = require('mini.ai').gen_spec.treesitter
-
-    require('mini.ai').setup {
-      n_lines = 500,
-      custom_textobjects = {
-        F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
-        c = spec_treesitter { a = '@class.outer', i = '@class.inner' },
-      },
     }
 
     -- MniFiles
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -18,39 +18,22 @@ return {
   build = ':TSUpdate',
   config = function()
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    vim.treesitter.language.register('markdown', 'octo')
+    vim.treesitter.language.register('markdown', 'octo') -- for using Octo.nvim
 
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
       ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown_inline', 'markdown', 'vim', 'vimdoc', 'ruby', 'sql' },
-      -- Autoinstall languages that are not installed
+
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
 
-      incremental_selection = {
-        enable = true,
-      },
+      -- TODO: need to setup treesitter textobject apporpirately
 
       endwise = {
         enable = true,
       },
     }
-
-    -- Remapping the keys again for better UX, since TS does not expose them
-    local is = require 'nvim-treesitter.incremental_selection'
-    -- vim.keymap.set('n', '<CR>', is.init_selection, { silent = true, desc = 'Init incremental selection' })
-    vim.keymap.set('v', '.', is.node_incremental, { silent = true, desc = 'Node incremental selection' })
-    vim.keymap.set('v', ',', is.node_decremental, { silent = true, desc = 'Node decremental selection' })
-    vim.keymap.set('v', 'g.', is.scope_incremental, { silent = true, desc = 'Scope incremental selection' })
-    -- Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
 
