a3056c105a973298899fc815733000f23af8f3dd
chuan2984 <chuanscareer@gmail.com>
2024-05-03 20:34:44 -0700

    fix(mini): change default mapping for surround to not conflict with flash

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -125,12 +125,17 @@ return {
       end,
     })
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- Mini Surround
+    require('mini.surround').setup {
+      mappings = {
+        add = 'gsa',
+        delete = 'gsd',
+        find = 'gsf',
+        find_left = 'gsF',
+        highlight = 'gsh',
+        replace = 'gsr',
+      },
+    }
 
     -- Mini AutoPairs
     require('mini.pairs').setup()
