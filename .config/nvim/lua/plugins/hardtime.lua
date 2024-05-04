547198c40b3f476326fb407d3213533f043e1d0a
chuan2984 <chuanscareer@gmail.com>
2024-05-03 18:17:51 -0700

    fix(hardtime): changed default restrictions

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,6 +1,11 @@
 return {
   'm4xshen/hardtime.nvim',
-  event = 'VeryLazy',
+  keys = { 'j', 'h', 'k', 'l' },
   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-  opts = {},
+  opts = {
+    max_time = 600,
+    max_count = 5,
+    disable_mouse = false,
+    disabled_filetypes = { 'qf', 'netrw', 'lazy', 'mason', 'oil' },
+  },
 }
