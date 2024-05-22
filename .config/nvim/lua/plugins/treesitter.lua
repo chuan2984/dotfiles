79213983f3a479f37fbc7424044a1cc8bd7677fa
chuan2984 <chuanscareer@gmail.com>
2024-05-22 11:56:31 -0700

    feat(treesitter): add new text object back, but may want to reconsider ai.mini

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,10 @@ return {
       'HiPhish/rainbow-delimiters.nvim',
       event = 'VeryLazy',
     },
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      event = 'BufRead',
+    },
   },
   build = ':TSUpdate',
   config = function()
@@ -29,11 +33,66 @@ return {
       indent = { enable = true },
 
       -- TODO: need to setup treesitter textobject apporpirately
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.outer',
+            ['ab'] = '@block.outer',
+            ['ib'] = '@block.inner',
+            -- You can also use captures from other query groups like `locals.scm`
+            ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@block.outer'] = 'V',
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+        },
+      },
 
       endwise = {
         enable = true,
       },
     }
+
+    local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+    -- Repeat movement with ; and ,
+    vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
+    vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
+
+    -- vim way: ; goes to the direction you were moving.
+    -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    local gs = require 'gitsigns'
+
+    -- make sure forward function comes first
+    local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
+    vim.keymap.set({ 'n', 'x', 'o' }, ']h', next_hunk_repeat)
+    vim.keymap.set({ 'n', 'x', 'o' }, '[h', prev_hunk_repeat)
+
+    local next_diag, prev_diag = ts_repeat_move.make_repeatable_move_pair(vim.diagnostic.goto_next, vim.diagnostic.goto_prev)
+    vim.keymap.set('n', '[d', next_diag, { desc = 'Go to previous [D]iagnostic message' })
+    vim.keymap.set('n', ']d', prev_diag, { desc = 'Go to next [D]iagnostic message' })
   end,
 }
 
