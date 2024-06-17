860ac1766ad6d4e62535550e4f89405a9de61fcb
chuan2984 <chuanscareer@gmail.com>
2024-06-17 14:58:54 -0700

    fix: add gpt4o in as a new agent

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -160,6 +160,46 @@ return {
   config = function()
     require('gp').setup {
       openai_api_key = { 'cat', os.getenv 'HOME' .. '/.openai_api_key' },
+      agents = {
+        {
+          name = 'ChatGPT4o',
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = 'gpt-4o',
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = 'You are a general AI assistant.\n\n'
+            .. 'The user provided the additional info about how they would like you to respond:\n\n'
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. '- Ask question if you need clarification to provide better answer.\n'
+            .. '- Think deeply and carefully from first principles step by step.\n'
+            .. '- Zoom out first to see the big picture and then zoom in to details.\n'
+            .. '- Use Socratic method to improve your thinking and coding skills.\n'
+            .. '- Try not to repeat previously presented answers.\n',
+        },
+        {
+          name = 'CodeGPT4',
+          chat = false,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = 'gpt-4-1106-preview', temperature = 0.8, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = 'You are an AI working as a code editor.\n\n'
+            .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
+            .. 'START AND END YOUR ANSWER WITH:\n\n```',
+        },
+        {
+          name = 'CodeGPT3-5',
+          chat = false,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = 'gpt-3.5-turbo-1106', temperature = 0.8, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = 'You are an AI working as a code editor.\n\n'
+            .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
+            .. 'START AND END YOUR ANSWER WITH:\n\n```',
+        },
+      },
       hooks = {
         -- example of adding command which writes unit tests for the selected code
         UnitTests = function(gp, params)
