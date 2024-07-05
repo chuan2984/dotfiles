ff2ad9eccabf6094d032826de121174af32e0801
chuan2984 <chuanscareer@gmail.com>
2024-07-05 15:31:57 -0700

    fix(ufo): reset fold column value

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -3,7 +3,7 @@ return {
   dependencies = 'kevinhwang91/promise-async',
   event = 'VeryLazy',
   config = function()
-    vim.opt.foldcolumn = '0' -- '0' is not bad
+    vim.opt.foldcolumn = '1' -- '0' is not bad
     vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.opt.foldlevelstart = 99
     vim.opt.foldenable = true
