eba05e2d7e6515620dbf68d3e7fa3d03ea6211ff
chuan2984 <chuanscareer@gmail.com>
2024-07-06 12:30:19 -0700

    fix(ufo): this seems to fix the always close problem in lua

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -3,12 +3,10 @@ return {
   dependencies = 'kevinhwang91/promise-async',
   event = 'VeryLazy',
   config = function()
-    vim.opt.foldcolumn = '1' -- '0' is not bad
-    vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    vim.opt.foldlevelstart = 99
+    vim.opt.foldcolumn = '0' -- '0' is not bad
+    vim.opt.foldlevel = 999
+    vim.opt.foldlevelstart = -1
     vim.opt.foldenable = true
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
     vim.keymap.set('n', 'zK', function()
       local winid = require('ufo').peekFoldedLinesUnderCursor()
       if not winid then
