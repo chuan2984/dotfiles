be42665b5220687fb8a3c593641e22648df25d77
chuan2984 <chuanscareer@gmail.com>
2024-07-24 11:43:47 -0700

    feat: add claude to gp.nvim

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -159,8 +159,24 @@ return {
   end,
   config = function()
     require('gp').setup {
-      openai_api_key = { 'cat', os.getenv 'HOME' .. '/.openai_api_key' },
+      -- openai_api_key = { 'cat', os.getenv 'HOME' .. '/.openai_api_key' },
+      providers = {
+        openai = {
+          secret = vim.fn.system('cat ' .. os.getenv 'HOME' .. '/.openai_api_key'),
+        },
+        anthropic = {
+          disable = false,
+          secret = vim.fn.system('cat ' .. os.getenv 'HOME' .. '/.anthropic_api_key'),
+        },
+      },
       agents = {
+        {
+          name = 'ChatGPT4o-mini',
+          chat = false,
+          command = true,
+          model = { model = 'gpt-4o', temperature = 1.1, top_p = 1 },
+          system_prompt = require('gp.defaults').chat_system_prompt,
+        },
         {
           name = 'ChatGPT4o',
           chat = true,
@@ -175,29 +191,30 @@ return {
             .. '- Think deeply and carefully from first principles step by step.\n'
             .. '- Zoom out first to see the big picture and then zoom in to details.\n'
             .. '- Use Socratic method to improve your thinking and coding skills.\n'
-            .. '- Try not to repeat previously presented answers.\n',
+            .. '- Try not to repeat previously presented answers.\n'
+            .. '- Do not be sorry if you get things wrong its ok.\n',
         },
         {
-          name = 'CodeGPT4',
-          chat = false,
-          command = true,
+          provider = 'anthropic',
+          name = 'ChatClaude-3-5-Sonnet',
+          chat = true,
+          command = false,
           -- string with model name or table with model name and parameters
-          model = { model = 'gpt-4-1106-preview', temperature = 0.8, top_p = 1 },
+          model = { model = 'claude-3-5-sonnet-20240620', temperature = 0.8, top_p = 1 },
           -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = 'You are an AI working as a code editor.\n\n'
-            .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
-            .. 'START AND END YOUR ANSWER WITH:\n\n```',
+          system_prompt = 'You are a general AI assistant.\n\n'
+            .. 'The user provided the additional info about how they would like you to respond:\n\n'
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. '- Ask question if you need clarification to provide better answer.\n'
+            .. '- Think deeply and carefully from first principles step by step.\n'
+            .. '- Zoom out first to see the big picture and then zoom in to details.\n'
+            .. '- Use Socratic method to improve your thinking and coding skills.\n'
+            .. '- Try not to repeat previously presented answers.\n'
+            .. '- Do not be sorry if you get things wrong its ok.\n',
         },
         {
-          name = 'CodeGPT3-5',
-          chat = false,
-          command = true,
-          -- string with model name or table with model name and parameters
-          model = { model = 'gpt-3.5-turbo-1106', temperature = 0.8, top_p = 1 },
-          -- system prompt (use this to specify the persona/role of the AI)
-          system_prompt = 'You are an AI working as a code editor.\n\n'
-            .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
-            .. 'START AND END YOUR ANSWER WITH:\n\n```',
+          name = 'ChatClaude-3-Haiku',
+          disable = true,
         },
       },
       hooks = {
