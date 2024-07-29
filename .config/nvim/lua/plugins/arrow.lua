497d825ed7e7de6a2973586326ba611d09104bb2
chuan2984 <chuanscareer@gmail.com>
2024-07-29 12:07:04 -0700

    fix: changed keybinding for saving line to something that does not interfere with default binding

--- .config/nvim/lua/plugins/arrow.lua
+++ .config/nvim/lua/plugins/arrow.lua
@@ -12,9 +12,9 @@ return {
 
     vim.keymap.set('n', 'H', require('arrow.persist').previous)
     vim.keymap.set('n', 'L', require('arrow.persist').next)
-    vim.keymap.set('n', '<C-s>', require('arrow.persist').toggle)
+    vim.keymap.set('n', "<C-'>", require('arrow.persist').toggle)
     vim.keymap.set('n', 'mn', '<cmd>Arrow next_buffer_bookmark<CR>')
     vim.keymap.set('n', 'mp', '<cmd>Arrow prev_buffer_bookmark<CR>')
-    vim.keymap.set('n', '<C-a>', '<cmd>Arrow toggle_current_line_for_buffer<CR>')
+    vim.keymap.set('n', '<C-s>', '<cmd>Arrow toggle_current_line_for_buffer<CR>')
   end,
 }
