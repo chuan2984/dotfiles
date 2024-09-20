51e3439196d9284d8448dd51211cda1dffc9487c
chuan2984 <chuanscareer@gmail.com>
2024-09-20 16:40:10 -0700

    fixup! feat: made find_file and git diff auto open if buffer is empty

--- .config/nvim/lua/plugins/smart-open.lua
+++ .config/nvim/lua/plugins/smart-open.lua
@@ -7,6 +7,17 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
+    require('telescope').setup {
+      extensions = {
+        smart_open = {
+          match_algorithm = 'fzf',
+          cwd_only = true,
+        },
+      },
+    }
+
     vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope smart_open<CR>', { noremap = true, silent = true, desc = '[] Smart find files' })
+
+    pcall(require('telescope').load_extension, 'smart_open')
   end,
 }
