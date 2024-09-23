e9a475a7a03bf55a71502e255657bb23af3a0229
chuan2984 <chuanscareer@gmail.com>
2024-09-23 14:39:31 -0700

    build(markdown-preview): add a way to preview md

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  -- install with yarn or npm
+  -- build = "cd app && yarn install",
+  -- init = function()
+  --   vim.g.mkdp_filetypes = { "markdown" }
+  -- end,
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
