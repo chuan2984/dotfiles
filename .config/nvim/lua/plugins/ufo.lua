530cebc5a4aa66403716f0f6993ee0eab62899db
chuan2984 <chuanscareer@gmail.com>
2024-09-25 15:14:39 -0700

    feat(ufo): replace eplipisses with the number of folded lines and make lazy load

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,27 +1,48 @@
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = 'kevinhwang91/promise-async',
+  keys = {
+    {
+      'zR',
+      function()
+        require('ufo').openAllFolds()
+      end,
+      { desc = 'open all folds' },
+    },
+    {
+      'zM',
+      function()
+        require('ufo').closeAllFolds()
+      end,
+      { desc = 'close all folds' },
+    },
+    {
+      'zr',
+      function()
+        require('ufo').openFoldsExceptKinds()
+      end,
+      { desc = 'close all folds except kinds' },
+    },
+    {
+      'zm',
+      function()
+        require('ufo').closeFoldsWith(vim.v.count)
+      end,
+      { desc = 'close folds with {v:count}' },
+    },
+    {
+      'zK',
+      function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end,
+      { desc = 'Peek Fold' },
+    },
+  },
   event = 'VeryLazy',
   config = function()
-    vim.keymap.set('n', 'zR', function()
-      require('ufo').openAllFolds()
-    end)
-    vim.keymap.set('n', 'zM', function()
-      require('ufo').closeAllFolds()
-    end)
-    vim.keymap.set('n', 'zr', function()
-      require('ufo').openFoldsExceptKinds()
-    end)
-    vim.keymap.set('n', 'zm', function()
-      require('ufo').closeFoldsWith(3)
-    end)
-    vim.keymap.set('n', 'zK', function()
-      local winid = require('ufo').peekFoldedLinesUnderCursor()
-      if not winid then
-        vim.lsp.buf.hover()
-      end
-    end, { desc = 'Peek Fold' })
-
     local ftMap = {
       vim = 'indent',
       python = { 'indent' },
@@ -49,7 +70,36 @@ return {
         end)
     end
 
+    local show_folded_line_num_handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, 'MoreMsg' })
+      return newVirtText
+    end
+
     require('ufo').setup {
+      fold_virt_text_handler = show_folded_line_num_handler,
       provider_selector = function(bufnr, filetype, buftype)
         return ftMap[filetype] or customizeSelector
       end,
