bc28ba10868385578dbe545cc25d42c61a43f3cd
chuan2984 <chuanscareer@gmail.com>
2024-11-26 20:46:19 -0800

    feat(dabod): add db explorer

---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,29 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = { 'tpope/vim-dadbod', lazy = true },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    vim.g.dbs = {
+      {
+        name = 'api_postgres',
+        url = 'postgresql://pgu:this_is_a_password@localhost:5432/postgres',
+      },
+      {
+        name = 'api_dev',
+        url = 'postgresql://pgu:this_is_a_password@localhost:5432/fieldwire_api_development',
+      },
+      {
+        name = 'api_redis',
+        url = 'redis://localhost:6379',
+      },
+    }
+
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
