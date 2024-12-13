3613414c68c15c742702e2d187229c466ff34f6a
chuan2984 <chuanscareer@gmail.com>
2024-12-12 20:11:01 -0800

    build: remove weechat

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,10 +1,10 @@
 return {
   'NeogitOrg/neogit',
   dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
-    'nvim-telescope/telescope.nvim', -- optional
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'nvim-telescope/telescope.nvim',
   },
-  cmd = 'Neogit',
+  cmd = { 'Neogit', 'DiffviewOpen', 'DiffviewFileHistory' },
   config = true,
 }
