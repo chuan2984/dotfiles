c77f13ea5ebd072d5af434b4c4b88cee4c17562b
chuan2984 <chuanscareer@gmail.com>
2024-12-15 10:00:37 -0800

    fix(conform): lengthen solargraph timeout

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 2500,
+      timeout_ms = 3500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
