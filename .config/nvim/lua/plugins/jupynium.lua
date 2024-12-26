f1265d72d71142c8cf4c1c44c10d6a010cb7b7fe
chuan2984 <chuanscareer@gmail.com>
2024-12-25 20:25:04 -0800

    fix(jupynium): change jupynium lazy event

--- .config/nvim/lua/plugins/jupynium.lua
+++ .config/nvim/lua/plugins/jupynium.lua
@@ -1,14 +1,11 @@
 return {
   'kiyoon/jupynium.nvim',
+  cmd = 'Jupynium',
   -- NOTE: to install the dependencies, one needs to create a virtualenv with Python3
   -- or with conda. Navigate to where this plugin is installed, ~/.local/share/nvim/lazy/jupynium.nvim
   -- make sure you have virtualenv activated, then run `pip3 install .`
   -- to make jupynium available systemwide, you can add `alias jupynium={path_to_bin_in_venv}` to your .zshrc
   -- build = 'pip3 install --user .',
-  cond = function()
-    local cwd = vim.fn.getcwd()
-    return cwd:match '/github/jupyter'
-  end,
   config = function()
     local cursorline_hl = vim.api.nvim_get_hl(0, { name = 'CursorLine' })
     local separator_bg = string.format('#%06x', cursorline_hl.bg - 0x101010)
