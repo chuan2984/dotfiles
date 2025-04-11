b5dd77d5be30b2628b255af8e4c94134fdd56f21
chuan2984 <chuanscareer@gmail.com>
2025-04-10 17:26:06 -0700

    fix(qmk): set lazy load on file match'

--- .config/nvim/lua/plugins/qmk.lua
+++ .config/nvim/lua/plugins/qmk.lua
@@ -1,19 +1,45 @@
 return {
   'codethread/qmk.nvim',
-  cmd = 'QMKFormat',
+  event = 'BufReadPre *keymap.c',
   config = function()
-    ---@type qmk.UserConfig
-    local conf = {
-      name = 'LAYOUT_5x6_5',
-      layout = {
-        '_ _ x x x x x x _ x x x x x x _ _',
-        '_ _ x x x x x x _ x x x x x x _ _',
-        '_ _ x x x x x x _ x x x x x x _ _',
-        '_ _ x x x x x x _ x x x x x x _ _',
-        '_ _ x x _ x x x _ x x x _ x x _ _',
-        '_ _ _ _ _ _ x x _ x x _ _ _ _ _ _',
-      },
-    }
-    require('qmk').setup(conf)
+    local group = vim.api.nvim_create_augroup('MyQMK', {})
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      desc = 'Format 5x6_5 keymap',
+      group = group,
+      pattern = '*5x6_5/keymaps/chuan/keymap.c',
+      callback = function()
+        require('qmk').setup {
+          name = 'LAYOUT_5x6_5',
+          auto_format_pattern = '*5x6_5/keymaps/chuan/keymap.c',
+          layout = {
+            'x x x x x x _ _ _ _ _ x x x x x x',
+            'x x x x x x _ _ _ _ _ x x x x x x',
+            'x x x x x x _ _ _ _ _ x x x x x x',
+            'x x x x x x _ _ _ _ _ x x x x x x',
+            '_ _ x x _ x x x _ x x x _ x x _ _',
+            '_ _ _ _ _ _ x x _ x x _ _ _ _ _ _',
+          },
+        }
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      desc = 'Format 4x6_3 keymap',
+      group = group,
+      pattern = '*4x6_3/keymaps/chuan/keymap.c',
+      callback = function()
+        require('qmk').setup {
+          name = 'LAYOUT_4x6_3',
+          auto_format_pattern = '*4x6_3/keymaps/chuan/keymap.c',
+          layout = {
+            'x x x x x x _ _ _ x x x x x x',
+            'x x x x x x _ _ _ x x x x x x',
+            'x x x x x x _ _ _ x x x x x x',
+            '_ _ x x x x x _ x x x x x _ _',
+          },
+        }
+      end,
+    })
   end,
 }
