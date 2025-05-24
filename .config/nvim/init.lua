11042fa0c3617d435371b57ef8d8843c4c0dabec
chuan2984 <chuanscareer@gmail.com>
2025-05-23 20:25:40 -0700

    fix(nvim): update basic settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 
 -- [[ Autocmd options ]]
 require 'autocmd'
