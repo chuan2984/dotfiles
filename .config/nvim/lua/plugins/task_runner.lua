9f9b55fa91f8eded833aed231721db9b2440f0b4
chuan2984 <chuanscareer@gmail.com>
2025-06-27 15:56:31 -0700

    fix(nvim): changed task runner to my own fork

--- .config/nvim/lua/plugins/task_runner.lua
+++ .config/nvim/lua/plugins/task_runner.lua
@@ -1,29 +1,27 @@
 return {
-  'miroshQa/rittli.nvim',
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
-  },
+  'chuan2984/rittli.nvim',
   keys = {
     {
       '<C-t>',
       function()
-        require('rittli.conveniences.terminal_tweaks').toggle_last_openned_terminal()
+        require('rittli.conveniences.terminal_tweaks').toggle_last_openned_terminal_wezterm()
       end,
-      mode = { 'n', 't' },
+      mode = { 'n', 'i', 'x' },
     },
     { '<Esc><Esc>', '<C-\\><C-n>', mode = 't' },
     {
       '<leader>tp',
       function()
-        require('rittli.core.telescope').tasks_picker()
+        Snacks.picker.rittli()
       end,
-      desc = 'Pick the task',
+      desc = 'task launcher',
     },
   },
   config = function()
     require('rittli').setup {
       terminal_provider = require('rittli.core.terminal_providers.wezterm').CreateMasterLayoutProvider(),
       folder_name_with_tasks = '.rittlitasks',
+
       -- turn this off for non neovim terminal
       conveniences = {
         enable = false,
