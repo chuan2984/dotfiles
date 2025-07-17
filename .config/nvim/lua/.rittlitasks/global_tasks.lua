30b8e2ab2dbcf5323580743c676dc223347af4f3
chuan2984 <chuanscareer@gmail.com>
2025-07-16 22:20:15 -0700

    feat(nvim): add d2 rendering tasks

--- .config/nvim/lua/.rittlitasks/global_tasks.lua
+++ .config/nvim/lua/.rittlitasks/global_tasks.lua
@@ -12,6 +12,29 @@ M.tasks = {
       return task
     end,
   },
+  {
+    name = 'Render current d2 diagram png',
+    builder = function()
+      local abs_file_path = vim.fn.expand '%:p'
+      local file_name = vim.fn.expand '%:t:r' .. '.png'
+      local task = {
+        cmd = { 'd2 ' .. abs_file_path .. ' --stdout-format=png - > ' .. file_name },
+      }
+
+      return task
+    end,
+  },
+  {
+    name = 'Watch current d2 diagram',
+    builder = function()
+      local abs_file_path = vim.fn.expand '%:p'
+      local task = {
+        cmd = { 'd2 -w --scale=1.3 ' .. abs_file_path },
+      }
+
+      return task
+    end,
+  },
 }
 
 return M
