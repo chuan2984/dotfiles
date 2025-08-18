626076eb90b4059624714740ec4b6630e171b66d
chuan2984 <chuanscareer@gmail.com>
2025-08-18 13:42:23 -0700

    fix(wezterm): add more themes

--- .config/wezterm/colorschemes/builtin_colorschemes.lua
+++ .config/wezterm/colorschemes/builtin_colorschemes.lua
@@ -1,20 +1,20 @@
 local M = {}
 
 local colorschemes = {
-	{ label = "Ayu Dark", value = "Ayu Dark (Gogh)" },
-	{ label = "Tokyo Night Moon", value = "Tokyo Night Moon" },
-	{ label = "Catppuccin Mocha", value = "Catppuccin Mocha" },
-	{ label = "Catppuccin Macchiato", value = "Catppuccin Macchiato" },
-	{ label = "Oxocarbon Dark", value = "Oxocarbon Dark (Gogh)" },
-	{ label = "Matrix", value = "matrix" },
+  { label = "Ayu Dark", value = "Ayu Dark (Gogh)" },
+  { label = "Tokyo Night Moon", value = "Tokyo Night Moon" },
+  { label = "Catppuccin Mocha", value = "Catppuccin Mocha" },
+  { label = "Catppuccin Macchiato", value = "Catppuccin Macchiato" },
+  { label = "Oxocarbon Dark", value = "Oxocarbon Dark (Gogh)" },
+  { label = "One Dark", value = "OneDark (base16)" },
 }
 
 M.init = function()
-	return colorschemes
+  return colorschemes
 end
 
 M.activate = function(config, _, value)
-	config.color_scheme = value
+  config.color_scheme = value
 end
 
 return M
